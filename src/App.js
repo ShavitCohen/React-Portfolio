b5c9099f59e7726f3d734fc26570bf826745a02c
Hagai Reuveni <arturdnt@gmail.com>
2019-09-07 15:50:14 +0300

    install banner

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import About from './components/About';
-// import Resume from './components/Resume';
 import Events from './components/Events';
 import Testimonials from  './components/Testimonials';
 import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
+import InstallBanner from './components/InstallBanner';
 import siteData from './siteData';
-export default () => (
-  <div className="App">
+
+export default () => {
+  const [installBannerActive, setInstallBannerState] = useState(true);
+  const removeBanner = () => setInstallBannerState(false);
+
+  return <div className="App">
     <Header siteData={siteData}/>
     <About siteData={siteData}/>
     <Events siteData={siteData}/>
-    {/* <Resume siteData={siteData}/> */}
     <Testimonials siteData={siteData}/>
     <ContactUs siteData={siteData}/>
     <Footer siteData={siteData}/>
+    {installBannerActive && <InstallBanner removeBanner={removeBanner}/>}
   </div>
-);
+}
---
+++ src/components/InstallBanner.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+
+export default ({ removeBanner }) => {
+    const [deferredPrompt, setDeferredPrompt] = useState(null);
+
+    useEffect(() => {
+        const registerBeforeInstallEvent = (e) => {
+            // Prevent Chrome 67 and earlier from automatically showing the prompt
+            e.preventDefault();
+            // See if the app is already installed, in that case, do nothing
+            if((window.matchMedia && window.matchMedia('(display-mode: standalone)').matches) || 
+                window.navigator.standalone === true) {
+                return false;
+            }
+            // Save the event so it can be triggered later.
+            setDeferredPrompt(e);
+        };
+        window.addEventListener('beforeinstallprompt', registerBeforeInstallEvent);
+        return () => window.removeEventListener("beforeinstallprompt", registerBeforeInstallEvent);
+    }, []);
+    
+    const handlePromptClick = async (e) => {
+        if (deferredPrompt) {
+            deferredPrompt.prompt();
+            const choiceResult = await deferredPrompt.userChoice;
+            if (choiceResult.outcome === 'accepted') {
+                console.log('Your PWA has been installed');
+            } else {
+                console.log('User chose to not install your PWA');
+            }
+            setDeferredPrompt(null);
+            removeBanner(false);
+        };
+    }
+
+    return deferredPrompt ? <div id="installBanner" onClick={handlePromptClick}>
+        Install the app
+    </div> : null;
+}
